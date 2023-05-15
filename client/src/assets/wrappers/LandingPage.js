f8a4bd25275be11346adc0fe74ead000cb38c77b
Brian Goodman <briangoodman@Brians-MacBook-Pro.local>
2023-05-15 13:06:07 -0400

    updated logos and icons

--- client/src/assets/wrappers/LandingPage.js
+++ client/src/assets/wrappers/LandingPage.js
@@ -1,22 +1,26 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.main`
+  background-color: var(--white);
   nav{
     width: var(--fluid-width);
-    max-width: var(--max-width);
+    max-width: 1125px;
     /* max-width: 570px; */
     margin: 0 auto;
     margin-top: 5rem;
     /* height: 300px; */
-    height: var(--nav-height);
+    // height: var(--nav-height);
+    height: 175px;
     display: flex;
     align-items: left;
+    // margin-left: 0rem;
   }
   .page{
     min-height: calc(90vh - var(--nav-height));
     display: grid;
     align-items: center;
     margin-top: -3rem;
+
   }
   h1{
     font-weight: 700;
